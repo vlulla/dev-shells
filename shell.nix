21131d389a3725e84ad6210cf21c90183a98c30e
Vijay Lulla <vijaylulla@gmail.com>
2021-02-20 18:24:24 +0000

    Create MANPATH env variable in a dev-shell.
    Also, use Nix's functional nature and builtin-functions to create this
    environment variable.

--- shell.nix
+++ shell.nix
@@ -5,8 +5,10 @@ pkgs.mkShell {
   buildInputs = [ which htop zlib jq coreutils findutils zstd lz4 fzf ripgrep sqlite ];
 
   shellHook = ''
-    echo hello
+    echo "Hello from shellHook!"
   '';
 
   MY_ENVIRONMENT_VARIABLE = "world";
+  MANPATH = builtins.concatStringsSep ":" (map (x: "${x}/share/man")
+              [ which htop findutils lz4 sqlite bash gnused gnutar gawk gnugrep diffutils patch ]);
 }
