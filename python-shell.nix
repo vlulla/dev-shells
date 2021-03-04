4540825b403448455f2a85edadeae8058e4db9d0
Vijay Lulla <vijaylulla@gmail.com>
2021-03-04 16:56:45 -0500

    Added property based testing package for python.

--- python-shell.nix
+++ python-shell.nix
@@ -2,7 +2,7 @@
 
 let
   pypkgs = with pkgs.python3Packages;
-    [ numpy simplejson requests matplotlib pyyaml pandas scikitlearn scikitimage ];
+    [ numpy simplejson requests hypothesis matplotlib pyyaml pandas scikitlearn scikitimage ];
 in
 pkgs.mkShell {
   name = "python-development-shell";
