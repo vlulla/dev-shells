a3602670459f67a29b9b0a2344d7c75e3f21d058
Vijay Lulla <vijaylulla@gmail.com>
2021-08-03 00:03:53 -0400

    fixed typos

--- r-shell.nix
+++ r-shell.nix
@@ -4,10 +4,10 @@ with pkgs.rPackages;
 let
   rpkgs = [ data_table ggplot2 simstudy ];
   additional-vlpkgs = [
-        Matrix RSQLite Rcpp SOAR biganalytics bigmemory bitabulate caret
+        Matrix RSQLite Rcpp SOAR biganalytics bigmemory bigtabulate caret
         digest doMC dplyr e1071 ff foreach gbm ggmap ggplot2 glmnet leaflet
         lpSolve mapview mlr3 nnet lidR ncdf4 jsonlite geonames igraph rnaturalearth
-        RNetCDF classInt parallel randomForest randtoolbox raster
+        RNetCDF classInt randomForest randtoolbox raster
         rbenchmark rgdal rgl SimMultiCorrData simstudy sf sp spdep sqldf
         stringi tau tidyverse tm xgboost xts zoo
      ];
@@ -15,6 +15,8 @@ in
 with pkgs;
 mkShell {
   name = "R-development-shell";
-  buildInputs = [ R ] ++ rpkgs ++ [ digest ];
+  buildInputs = [ R ] ++ rpkgs ++ [ digest ]
+   /* ++ additional-vlpkgs */
+  ;
   MANPATH="${R.outPath}/share/man";
 }
